c11d7020d162f5cef77c2cfd14829a2ac6876bef
alessandrob86 <alessandro@centroricambiautosrl.it>
2026-03-20 16:57:00 +0100

    feat: restore standard routing and keep both Home and Promo pages active

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,10 @@ import { HomePage } from './pages/HomePage';
 import { PromoPage } from './pages/PromoPage';
 
 function App() {
-  const isRegistrazioni = window.location.hostname.includes('registrazioni');
-
   return (
     <Router>
       <Routes>
-        <Route path="/" element={isRegistrazioni ? <PromoPage /> : <HomePage />} />
+        <Route path="/" element={<HomePage />} />
         <Route path="/promo" element={<PromoPage />} />
       </Routes>
     </Router>
